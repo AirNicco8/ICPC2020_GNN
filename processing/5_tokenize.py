c409ea6142084ad05cb5c69ab02eb6c5113ebfe6
AirNicco8 <lornic98@gmail.com>
2022-02-03 16:53:14 +0100

    edited 5_tokenize to create smls.tok, added build

--- processing/5_tokenize.py
+++ processing/5_tokenize.py
@@ -6,18 +6,11 @@ coms_vocab = 10908 # orginally was 44707
 tdats_vocab = 75000
 sml_vocab = 75000
 
-comsfile = './output/dataset.coms'
-comstok_file = 'coms.tok'
-
-tdatsfile = './output/dataset.tdats'
-tdatstok_file = 'tdats.tok'
-
-
 smlfile = './output/dataset.srcml.seq'
 sml_vocab = sml_vocab # sml vocab is small so just pick a big number
-smltok_file = 'smls.tok'
+smltok_file = 'output/smls.tok'
 
-p = pickle.load(open('tdats.tok', 'rb'), encoding='UTF-8')
+p = pickle.load(open('output/tdats.tok', 'rb'), encoding='UTF-8')
 
 q = p
 q.update_from_file(smlfile)
